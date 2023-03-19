5146d92f8935e227bd4c04bfe3024a9ff6660f19
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-03-19 07:59:25 +0300

    .

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,6 +4,10 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import NavbarComponent from "../components/NavbarComponent";
 import Image from "next/image";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
 import furrycare from "../images/furrycare.svg";
 
 const index = () => {
@@ -41,11 +45,30 @@ const index = () => {
         }}
       >
         {" "}
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-        <Button sx={{ width: "100%" }} variant="contained">
-          Contained
+        <TextField id="standard-basic" label="Name" variant="standard" />
+        <TextField id="standard-basic" label="Email" variant="standard" />
+        <TextField id="standard-basic" label="Password" variant="standard" />
+        <Box>
+          <FormControl>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+            >
+              <FormControlLabel
+                value="owner"
+                control={<Radio />}
+                label="Owner"
+              />
+              <FormControlLabel value="vet" control={<Radio />} label="Vet" />
+            </RadioGroup>
+          </FormControl>
+        </Box>
+        <Button sx={{ width: "100%", marginTop: "1rem" }} variant="contained">
+          LogIn
+        </Button>
+        <Button sx={{ width: "100%", marginTop: "2rem" }} variant="contained">
+          SignUp
         </Button>
       </Box>
     </div>
