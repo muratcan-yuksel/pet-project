973bac3d048abd13438ef1f108f05008e372dab5
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-03-20 06:59:33 +0300

    pets created

--- frontend/src/components/Pets.js
+++ frontend/src/components/Pets.js
@@ -1,16 +1,236 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { TextField } from "@mui/material";
+import axios from "axios";
+import { DataContext } from "@/context/provider";
 
 const pets = ({ pets }) => {
   console.log(pets);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [breed, setBreed] = useState("");
+  const [category, setCategory] = useState("");
+  const [allergies, setAllergies] = useState("");
+  const [bloodGroup, setBloodGroup] = useState("");
+  const [weight, setWeight] = useState("");
+  const [currentMedications, setCurrentMedications] = useState("");
+  const [vaccinationHistory, setVaccinationHistory] = useState("");
+  const [medicalHistoryDate, setMedicalHistoryDate] = useState("");
+  const [medicalHistoryNote, setMedicalHistoryNote] = useState("");
+  const [medicalHistoryVet, setMedicalHistoryVet] = useState("");
+
+  const [data, setData] = useContext(DataContext);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleAgeChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const handleBreedChange = (event) => {
+    setBreed(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleAllergiesChange = (event) => {
+    setAllergies(event.target.value);
+  };
+
+  const handleBloodGroupChange = (event) => {
+    setBloodGroup(event.target.value);
+  };
+
+  const handleWeightChange = (event) => {
+    setWeight(event.target.value);
+  };
+
+  const handleCurrentMedicationsChange = (event) => {
+    setCurrentMedications(event.target.value);
+  };
+
+  const handleVaccinationHistoryChange = (event) => {
+    setVaccinationHistory(event.target.value);
+  };
+
+  const handleMedicalHistoryDate = (event) => {
+    setMedicalHistoryDate(event.target.value);
+  };
+  const handleMedicalHistoryNote = (event) => {
+    setMedicalHistoryNote(event.target.value);
+  };
+  const handleMedicalHistoryVet = (event) => {
+    setMedicalHistoryVet(event.target.value);
+  };
+
+  const createPet = async (e) => {
+    e.preventDefault();
+    const formData = {
+      name,
+      age,
+      breed,
+      owner: data.user._id,
+      category,
+      allergies,
+      bloodGroup,
+      weight,
+      currentMedications,
+      vaccinationHistory,
+      medicalHistoryDate,
+      medicalHistoryNote,
+      medicalHistoryVet,
+    };
+
+    console.log(formData);
+    try {
+      const res = await axios.post("http://localhost:3000/pets", formData, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          overflow: "hidden",
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="name"
+          variant="outlined"
+          sx={{ margin: "1rem" }}
+          value={name}
+          onChange={handleNameChange}
+        />
+        <TextField
+          id="outlined-basic"
+          label="age"
+          variant="outlined"
+          sx={{ margin: "1rem" }}
+          value={age}
+          onChange={handleAgeChange}
+        />
+        <TextField
+          id="outlined-basic"
+          label="breed"
+          variant="outlined"
+          sx={{ margin: "1rem" }}
+          value={breed}
+          onChange={handleBreedChange}
+        />
+        <TextField
+          id="outlined-basic"
+          label="category"
+          variant="outlined"
+          sx={{ margin: "1rem" }}
+          value={category}
+          onChange={handleCategoryChange}
+        />
+        <TextField
+          id="outlined-basic"
+          label="allergies"
+          variant="outlined"
+          sx={{ margin: "1rem" }}
+          value={allergies}
+          onChange={handleAllergiesChange}
+        />
+        <TextField
+          id="outlined-basic"
+          label="bloodGroup"
+          variant="outlined"
+          sx={{ margin: "1rem" }}
+          value={bloodGroup}
+          onChange={handleBloodGroupChange}
+        />
+        <TextField
+          id="outlined-basic"
+          label="weight"
+          variant="outlined"
+          sx={{ margin: "1rem" }}
+          value={weight}
+          onChange={handleWeightChange}
+        />
+        <TextField
+          id="outlined-basic"
+          label="currentMedications"
+          variant="outlined"
+          sx={{ margin: "1rem" }}
+          value={currentMedications}
+          onChange={handleCurrentMedicationsChange}
+        />
+        <TextField
+          id="outlined-basic"
+          label="vaccinationHistory"
+          variant="outlined"
+          sx={{ margin: "1rem" }}
+          value={vaccinationHistory}
+          onChange={handleVaccinationHistoryChange}
+        />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="medicalHistory"
+            variant="outlined"
+            sx={{ margin: "1rem" }}
+            value={medicalHistoryDate}
+            onChange={handleMedicalHistoryDate}
+          />{" "}
+          <TextField
+            id="outlined-basic"
+            label="medicalHistory"
+            variant="outlined"
+            sx={{ margin: "1rem" }}
+            value={medicalHistoryNote}
+            onChange={handleMedicalHistoryNote}
+          />{" "}
+          <TextField
+            id="outlined-basic"
+            label="medicalHistory"
+            variant="outlined"
+            sx={{ margin: "1rem" }}
+            value={medicalHistoryVet}
+            onChange={handleMedicalHistoryVet}
+          />
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button onClick={createPet} variant="contained">
+          Add Pet
+        </Button>
+      </Box>
+
       {pets &&
         pets.map((pet) => (
           <Box
